d87212ff2ccaf55fefa1933c0908b5a20ede3c0b
Alexey Khalaydzhi <aleksei_halaidzh@mail.ru>
2022-03-12 00:13:19 +0300

    Updated base docker container and linters example

--- 10/linters/run.sh
+++ 10/linters/run.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/env bash
 
-set -e
 set -o pipefail
 
 function print_header() {
     echo -e "\n***** ${1} *****"
 }
 
+function check_log() {
+    LOG=$( { ${1}; } 2>&1 )
+    STATUS=$?
+    echo "$LOG"
+    if echo "$LOG" | grep -q -E "${2}"
+    then
+        exit 1
+    fi
+
+    if [ $STATUS -ne 0 ]
+    then
+        exit $STATUS
+    fi
+}
+
 print_header "RUN cppcheck"
-#cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include
+check_log "cppcheck project --enable=all --inconclusive --error-exitcode=1 -I project/include --suppress=missingIncludeSystem" "\(information\)"
 
 print_header "RUN clang-tidy"
-#clang-tidy project/**/* -- -Iproject/include
+check_log "clang-tidy project/src/* project/include/* -warnings-as-errors=* -extra-arg=-std=c99 -- -Iproject/include" "Error (?:reading|while processing)"
 
-print_header "RUN cpplint.py"
-python3 ./linters/cpplint/cpplint.py --extensions=c project/include/* project/src/*
+print_header "RUN cpplint"
+check_log "cpplint --extensions=c project/include/* project/src/*" "Can't open for reading"
 
 print_header "SUCCESS"
